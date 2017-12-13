7ff1df8381f566c81d66c3a6526751354d39a5b2
Elina Hovakimyan <elinahovakimyan@gmail.com>
2017-12-13 17:31:48 +0400

    Process port

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     devServer: {
         inline: true,
         contentBase: './dist',
-        port: 3000
+        port: process.env.port || 3000
     },
     module: {
         loaders: [
